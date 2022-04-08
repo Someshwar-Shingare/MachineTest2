0908f8563cfba18a5b65aa97273fcbf17434a4c1
Someshwar-Shingare <someshshingare@gmail.com>
2022-04-08 11:11:46 +0530

    test

--- Project1/testapp/models.py
+++ Project1/testapp/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     book_name = models.CharField(max_length=32)
     book_description = models.CharField(max_length=30)
 
 
 class Author(models.Model):
-    id = models.IntegerField(primary_key=True)
+
     auth_name = models.CharField(max_length=32)
     email = models.CharField(max_length=30)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, default='')
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True, blank=True)
 
 
 class Category(models.Model):
-    id = models.IntegerField(primary_key=True)
+
 
     cat_name = models.CharField(max_length=32)
 
